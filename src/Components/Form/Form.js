43c37e10b8ec0babb90142f39b1c96b108f7c192
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-26 17:48:29 -0800

    created loading state

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,11 +1,11 @@
-export default function Form() {
+export default function Form({ selectedThing, setSelectedThing, villagers, fish, bugs, art }) {
   return (
     <div>
-      <select name="things">
-        <option value="villagers">Villagers</option>
-        <option value="fish">Fish</option>
-        <option value="bugs">Bugs</option>
-        <option value="art">Art</option>
+      <select value={selectedThing} onChange={(e) => setSelectedThing(e.target.value)}>
+        <option value={villagers}>Villagers</option>
+        <option value={fish}>Fish</option>
+        <option value={bugs}>Bugs</option>
+        <option value={art}>Art</option>
       </select>
     </div>
   );
--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -4,20 +4,26 @@ import List from '../../Components/List/List';
 import Form from '../../Components/Form/Form';
 export default function Home() {
   const [villagers, setVillagers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedThing, setSelectedThing] = useState('villagers');
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getVillagers();
       setVillagers(data);
+      setLoading(false);
     };
-    fetchData();
-  }, []);
+    if (loading) {
+      fetchData();
+    }
+  }, [loading]);
 
   return (
     <>
       <div>Welcome Home</div>
-      <Form />
-      <List villagers={villagers} />
+      <Form setLoading={setLoading} />
+      {loading && <span>Loading...</span>}
+      {!loading && <List villagers={villagers} loading={loading} />}
     </>
   );
 }
