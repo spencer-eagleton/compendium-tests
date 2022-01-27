ca39a8a4502969c5a2dab27b98fd1581ab6e0700
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-26 22:55:01 -0800

    began second test

--- src/Views/Home.test.js
+++ src/Views/Home.test.js
@@ -1,10 +1,16 @@
 import { render, screen } from '@testing-library/react';
 
 import Home from './Home/Home';
+import userEvent from '@testing-library/react';
 
 test('renders welcome home on home page', () => {
   render(<Home />);
 
   const welcome = screen.getByText(/welcome home/i);
   expect(welcome).toBeInTheDocument();
+});
+
+test('select box works', async () => {
+  render(<Home />);
+  //   const select = await screen.findByRole('combobox');
 });
